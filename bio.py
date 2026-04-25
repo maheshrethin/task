97c4282855ceed09d9c0972b960693d39ed1e895
maheshrethin <maheshrethin@gmail.com>
2026-04-25 20:46:59 +0530

    first commit

---
+++ bio.py
@@ -0,0 +1,22 @@
+name = "aswathi"
+height = 5.4
+weight = 55
+email = "maheshrethin@gmail.com"
+skills = "python"
+hobby = "reading books"
+education = "class 12th "
+location = "ponnamapet"
+language = "English, tamil"
+phone="1234567890"
+
+print("bio data.....")
+print("name is", name)
+print("height is", height)
+print("weight is", weight)
+print("email is", email)
+print("skills is", skills)
+print("hobby is", hobby)
+print("education is", education)
+print("location is", location)
+print("language is", language)
+print("phone is", phone)
